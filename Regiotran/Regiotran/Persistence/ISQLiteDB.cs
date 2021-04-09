51014802a452f0966d340cf38a32d23693c716ef
Juan Sebastian Quintero <juansquintero@ucundinamarca.edu.co>
2021-04-09 08:43:10 -0500

    Bad implementation

---
+++ Regiotran/Regiotran/Persistence/ISQLiteDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Regiotran.Persistence
+{
+    public interface ISQLiteDB
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
