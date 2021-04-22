087dd831c47f220d9b174a9f96a354e11940a1b5
DAVID RODRIGUEZ <dsebastianrodriguez@ucundinamarca.edu.co>
2021-04-22 11:52:36 -0500

    Se implemento el lector de codigoQR, en Master Page se implemento dos lineas de codigo necesarias para el funcionamiento del mismo y ademas se agrego un nuevo nuget "Zxing"
    La vista que se implemento para esto se llama "LectorCodeQR" eso fue todo

---
+++ Regiotran/Regiotran/Views/LectorCodeQR.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace Regiotran.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LectorCodeQR : ContentView
+    {
+        public LectorCodeQR()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScannerQR_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+                scanner.TopText = "Lector codigo QR \n Regiotran";
+                scanner.BottomText = "Lea su codigo ahora";
+                var result = await scanner.Scan();
+                if (result != null)
+                {
+                    txtResultado.Text = result.Text;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
